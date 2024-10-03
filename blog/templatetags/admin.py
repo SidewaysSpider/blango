86e1485a37cb3d47e365410b8c21ed4db909dc66
Jeffrey Baron <jeffbaron899@gmail.com>
2024-10-03 23:02:45 +0000

    Finish crispy forms

--- blog/templatetags/admin.py
+++ blog/templatetags/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Tag, Post, Comment
+from blog.models import Tag, Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-"""
+
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
-"""
