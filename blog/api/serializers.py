640b875a7f6804a984e6da2c5106f4db0eb5257c
Jeffrey Baron <jeffbaron899@gmail.com>
2024-10-21 18:03:52 +0000

    Finish related fields

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Post, Tag
+from blango_auth.models import User
+import logging
+logger = logging.getLogger(__name__)
 
 class PostSerializer(serializers.ModelSerializer):
+    #tags and author are defined so that when the blog.api.views.PostList.as_view()  
+    #and blog.api.views.PostDetail.as_view() methods are exectuted what is returned is
+    #are meaningful values rather than just the primary keys.  For example, if tags
+    #wasn't defined as below, the tags field in the output permission_classes
+    #per https://blah-8000.codio.io/api/v1/posts/1
+    #would be:
+    #"tags": [1]
+    #rather than 
+    #"tags": ["Bondos"]
+
+    tags = serializers.SlugRelatedField(
+        slug_field="value", many=True, queryset=Tag.objects.all()
+    )
+    
+    author = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(), view_name="api_user_detail", lookup_field="email"
+    )
+    logger.debug("type of tags is %s",type(tags))
+    logger.debug(dir(tags))
+    logger.debug("type of author is %s",type(author))
+    logger.debug(dir(author))
     class Meta:
         model = Post
         fields = "__all__"
         readonly = ["modified_at", "created_at"]
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
 
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,17 +1,32 @@
 from rest_framework import generics
 
-from blog.api.serializers import PostSerializer
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
 #from blog.api.permissions import AuthorModifyOrReadOnly
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
+import logging
+logger = logging.getLogger(__name__)
+
 class PostList(generics.ListCreateAPIView):
+    logger.debug("in blog.api.views.PostList")
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     #permission_classes = [AuthorModifyOrReadOnly]
+    logger.debug("in blog.api.views.PostDetail")
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    logger.debug("In blog.api.views.UserDetail and")
+    logger.debug("queryset[0] and queryset[1] are")
+    logger.debug(queryset[0])
+    logger.debug(queryset[1])
+    serializer_class = UserSerializer
