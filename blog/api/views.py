438f6750b884c1f55b5f0172d6abb89d1a27073c
Jeffrey Baron <jeffbaron899@gmail.com>
2024-11-08 19:01:52 +0000

    Finish filtering

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,4 +1,9 @@
 from rest_framework import generics, viewsets
+from django.db.models import Q
+from django.utils import timezone
+
+from datetime import timedelta
+from django.http import Http404
 
 from blango_auth.models import User
 #from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
@@ -221,6 +226,49 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
 
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            # published only
+            #return self.queryset.filter(published_at__lte=timezone.now())
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
+        elif self.request.user.is_staff:
+            # allow all
+            #return self.queryset
+            queryset = self.queryset
+
+        # filter for own or
+        else:
+          """
+          return self.queryset.filter(
+            Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+          )
+          """
+          queryset = self.queryset.filter(
+            Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+          )
+
+        time_period_name = self.kwargs.get("period_name")
+
+        if not time_period_name:
+            # no further filtering required
+            return queryset
+
+        if time_period_name == "new":
+            return queryset.filter(
+                published_at__gte=timezone.now() - timedelta(hours=1)
+            )
+        elif time_period_name == "today":
+            return queryset.filter(
+                published_at__date=timezone.now().date(),
+            )
+        elif time_period_name == "week":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+        else:
+            raise Http404(
+                f"Time period {time_period_name} is not valid, should be "
+                f"'new', 'today' or 'week'"
+            )
+
     def get_serializer_class(self):
         if self.action in ("list", "create"):
             return PostSerializer
@@ -237,6 +285,13 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer = PostSerializer(posts, many=True, context={"request": request})
         return Response(serializer.data)
 
+    """
     @method_decorator(cache_page(120))
+    def list(self, *args, **kwargs):
+        return super(PostViewSet, self).list(*args, **kwargs)
+    """
+
+    @method_decorator(cache_page(120))
+    @method_decorator(vary_on_headers("Authorization", "Cookie"))
     def list(self, *args, **kwargs):
         return super(PostViewSet, self).list(*args, **kwargs)
