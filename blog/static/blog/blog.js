0e1d221709793734bdc4ebfe19277c0724dfc7f2
Jeffrey Baron <jeffbaron899@gmail.com>
2024-11-14 19:57:59 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -67,8 +67,6 @@ console.log('Here are the doubled numbers')
 
 console.log(doubled)
 
-*/
-
 class Greeter {
   constructor (name) {
     this.name = name
@@ -118,3 +116,75 @@ dg2.greet()
 
 const dg1 = new DelayedGreeter('Stroller 1 Second', 1000)
 dg1.greet()
+
+*/
+
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
+}
+
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
+}
+
+/*
+The function that other code will call is lazyAdd, but we define 
+another function inside it called doAdd which contains the actual 
+code to do the addition. We need to do this because the function 
+we pass to the Promise class must only take resolve and reject 
+functions as parameters. We could not pass a function to Promise 
+that takes the numbers to add and the resolve and reject functions.
+
+Below doAdd is the function that will be passed to
+the Promise class.  doAdd takes resolve and reject functions as
+parameters.
+*/
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
+    }
+  }
+  /*
+   The function we pass to the Promise class must only take 
+   resolve and reject functions as parameters.  Below, we 
+   instantiate a promise using doAdd as the parameter.
+  */
+  return new Promise(doAdd)
+}
+
+/*
+Below lazyAdd(3,4) returns a Promise object to p.  This is per the 
+return new Promise(doAdd) at the end of the lazyAdd function.
+See above.  
+lazyAdd is the first callback. See below for the other two callbacks.
+What we’ve defined so far could be considered the “producer” 
+side of the Promise system.
+*/
+
+const p = lazyAdd(3, 4)
+
+/*
+We need to define two more callback functions. We know from documentation,
+or inspecting source code, that the resolve function accepts a single
+argument, the result of the addition. We’d also know the reject function
+also accepts a single argument, a string that describes why the promise
+was rejected. It is up to us as the consumer to decide how to deal 
+with the results.
+
+How do we actually get the lazyAdd() function to give us a result? We use
+the then() method on a Promise. The then() method has the same signature 
+as whatever function was passed to the Promise constructor. In our case, 
+this was the doAdd() internal function, and we know it accepts resolve 
+and reject callback functions. Once the then() method is called, 
+whether it resolves or rejects, the Promise is then called (considered)
+settled.
+*/
+
+//resolvedCallback and rejectedCallbackk are the other two callback functions
+p.then(resolvedCallback, rejectedCallback)
+
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
